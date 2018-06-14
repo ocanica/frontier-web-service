dd12f5609a958760a3f75492e297c447cc350bc9
Lloyd Headbush <ocanica@gmail.com>
2018-06-14 16:49:55 +0100

    Undone hook test changes

--- templates/landing.php
+++ templates/landing.php
@@ -1,6 +1,6 @@
         <section id="landing">
             <div id="intro">
-                <h1 class="d1">end and receive money from more frontiers around the world.</h1>
+                <h1 class="d1">Send and receive money from more frontiers around the world.</h1>
                 <h1 class="d2">Wysyłaj i odbieraj pieniądze z większej liczby granic na całym świecie.</h1>
                 <h1 class="d2">दुनिया भर के अधिक सीमाओं से धन भेजें और प्राप्त करें</h1>
                 <br></br>
